00258d62c0e8b9c8942c27623f9a8de86e997084
Ako090921 <92437511+Ako090921@users.noreply.github.com>
2021-10-13 19:18:38 +0800

    Add files via upload

---
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MARC
+ */
+public class JavaApplication12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+         System.out.println("WHERE DO YOU WANT TO EAT?");
+            System.out.println("--------------------------------");
+            System.out.println("01.RESTAURANT NI ALING NENA");
+            System.out.println("02.ABEY ROAD CAFE");
+            System.out.println("03.PINK FLOYD'S HOUSE");
+        int x = scan.nextInt();
+        
+        if(x == 1){
+            System.out.println("RESTAURANT NI ALING NENA");
+          System.out.println("-----------------------------");
+          System.out.println("~~MENU~~");
+          System.out.println("5.SINIKMURAANG ULO NG MANOK");
+          System.out.println("6.INIHAW NA ULING");
+          System.out.println("7.PRINITONG TUBIG");
+          System.out.println("8.NILAGANG MANTIKA");
+          System.out.println("9.SINABAWANG GAAS");
+          int f = scan.nextInt();
+        if(f>= 5 && f<= 5){
+        System.out.println("~~YOU CHOOSE SINIKMURAANG ULO NG MANOK");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        }
+        if(f>= 6 && f<= 6){
+        System.out.println("~~YOU CHOOSE INIHAW NA ULING");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        if(f>= 7 && f<= 7){
+        System.out.println("~~YOU CHOOSE PRINITONG TUBIG");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        }
+        if(f>= 8 && f<= 8){
+        System.out.println("~~YOU CHOOSE NILAGANG MANTIKA");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        if(f>= 9 && f<= 9){
+        System.out.println("~~YOU CHOOSE SINABAWANG GAAS");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        }
+        
+        if(x == 2){
+            System.out.println("ABEY ROAD CAFE");
+          System.out.println("-----------------------------");
+          System.out.println("~~MENU~~");
+          System.out.println("10.SINIKMURAANG ULO NG MANOK");
+          System.out.println("11.INIHAW NA ULING");
+          System.out.println("12.PRINITONG TUBIG");
+          System.out.println("13.NILAGANG MANTIKA");
+          System.out.println("14.SINABAWANG GAAS");
+          int g = scan.nextInt();
+        if(g>= 10 && g<= 10){
+        System.out.println("~~YOU CHOOSE SINIKMURAANG ULO NG MANOK");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        }
+        if(g>= 11 && g<= 11){
+        System.out.println("~~YOU CHOOSE INIHAW NA ULING");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        if(g>= 12 && g<= 12){
+        System.out.println("~~YOU CHOOSE PRINITONG TUBIG");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        }
+        if(g>= 13 && g<= 13){
+        System.out.println("~~YOU CHOOSE NILAGANG MANTIKA");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        if(g>= 14 && g<= 14){
+        System.out.println("~~YOU CHOOSE SINABAWANG GAAS");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        }
+        
+        if(x == 3){
+            System.out.println("PINK FLOYD'S HOUSE");
+          System.out.println("-----------------------------");
+          System.out.println("~~MENU~~");
+          System.out.println("15.SINIKMURAANG ULO NG MANOK");
+          System.out.println("16.INIHAW NA ULING");
+          System.out.println("17.PRINITONG TUBIG");
+          System.out.println("17.NILAGANG MANTIKA");
+          System.out.println("19.SINABAWANG GAAS");
+          int h = scan.nextInt();
+        if(h>= 15 && h<= 15){
+        System.out.println("~~YOU CHOOSE SINIKMURAANG ULO NG MANOK");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        }
+        if(h>= 16 && h<= 16){
+        System.out.println("~~YOU CHOOSE INIHAW NA ULING");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        if(h>= 17 && h<= 17){
+        System.out.println("~~YOU CHOOSE PRINITONG TUBIG");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        }
+        if(h>= 18 && h<= 18){
+        System.out.println("~~YOU CHOOSE NILAGANG MANTIKA");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        if(h>= 19 && h<= 19){
+        System.out.println("~~YOU CHOOSE SINABAWANG GAAS");
+        System.out.println("~~THANK YOU FOR YOUR PURCHASE~~");
+        } 
+        }     
+    }
+}
